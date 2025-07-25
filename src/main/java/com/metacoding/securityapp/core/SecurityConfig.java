d66c23518bda243fb795689cd9ef16a1b775ed7d
getinthere <getinthere@naver.com>
2025-07-25 11:02:31 +0900

    권한 추가

--- src/main/java/com/metacoding/securityapp/core/SecurityConfig.java
+++ src/main/java/com/metacoding/securityapp/core/SecurityConfig.java
@@ -19,6 +19,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // H2 Console은 iframe으로 동작하므로, 이 설정이 필요합니다.
+        http.headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions.sameOrigin()));
+
         http.csrf(configure -> configure.disable());
 
         http.formLogin(form -> form
@@ -28,9 +32,12 @@ public class SecurityConfig {
 
         );
 
+        // /main 인증이 필요해, /admin ADMIN 권한이 필요해, /user USER 권한이 필요해
         http.authorizeHttpRequests(
                 authorize -> authorize
-                        .requestMatchers("/user/**", "/main").authenticated()
+                        .requestMatchers("/main").authenticated()
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
         );
 
